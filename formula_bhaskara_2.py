b4f7a4b895a80d750b101ac961ca266d4f4e2fde
Rogério Carmo <rogeriocarmo.ti@gmail.com>
2023-06-26 14:46:36 -0400

    Arquivo .py da Equação do 2º Grau
    Realiza o cálculo de x1 e x2 da Equação do 2º Grau.

---
+++ formula_bhaskara_2.py
@@ -0,0 +1,22 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+# Fórmula de Bhaskara
+
+print("***FÓRMULA DE BHASKARA****")
+a = float(input('Entre com o valor de a: '))
+b = float(input('Entre com o valor de b: '))
+c = float(input('Entre com o valor de c: '))
+
+delta = (b ** 2) - 4 * a * c
+
+print("\n**************************\n")
+
+if a == 0:
+    print("O valor de a, deve ser diferente de 0")
+elif delta < 0:
+    print("Sem raízes reais")
+else:
+    x1 = (-b + delta ** (1 / 2)) / (2 * a)
+    x2 = (-b - delta ** (1 / 2)) / (2 * a)
+
+    print("x1: {}, x2: {}".format(x1, x2))
